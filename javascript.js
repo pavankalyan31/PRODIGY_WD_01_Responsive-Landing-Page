e4bbf21843b03e6a0ade92dd4f45f2073684ebba
pavankalyan31 <pavankalyanssmunagala@gmail.com>
2024-08-20 17:03:16 +0530

    first commit

---
+++ javascript.js
@@ -0,0 +1,8 @@
+document.addEventListener('scroll', function() {
+    const navbar = document.getElementById('navbar');
+    if (window.scrollY > 50) {
+        navbar.classList.add('scrolled');
+    } else {
+        navbar.classList.remove('scrolled');
+    }
+});
